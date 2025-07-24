04af03311167eb3780db4bf9ebbc0f8a8c79ea35
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-07-25 03:15:22 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,56 +1,58 @@
 let allQuestion = [
 
-       {
-          question : "HTML stand for?",
-          option : [
-            {answer : "Hypertext Makeup Language", isCorrect : false },
-            {answer : "Hypertext Make Language", isCorrect : false },
-            {answer : "Hypertext Markup Language", isCorrect : true },
-          ]
-       },
+    {
+        question: "HTML stand for?",
+        option: [
+            { answer: "Hypertext Makeup Language", isCorrect: false },
+            { answer: "Hypertext Make Language", isCorrect: false },
+            { answer: "Hypertext Markup Language", isCorrect: true },
+        ]
+    },
 
     {
-       question : "HTML and HTML5 difference?",
-       option : [
-         {answer : "all element supported", isCorrect : false },
-         {answer : "HTML does not supported audio vedio element", isCorrect : true },
-         {answer : "HTML does supported audio vedio element", isCorrect : false },
-       ]
+        question: "HTML and HTML5 difference?",
+        option: [
+            { answer: "all element supported", isCorrect: false },
+            { answer: "HTML does not supported audio vedio element", isCorrect: true },
+            { answer: "HTML does supported audio vedio element", isCorrect: false },
+        ]
     },
     {
-      question: "Which tag is used to create a hyperlink in HTML?",
-      option: [
-        { answer: "<a>", isCorrect: true },
-        { answer: "<link>", isCorrect: false },
-        { answer: "<href>", isCorrect: false },
-      ]
+        question: "Which tag is used to create a hyperlink in HTML?",
+        option: [
+            { answer: "a", isCorrect: true },
+            { answer: "link", isCorrect: false },
+            { answer: "href", isCorrect: false },
+        ]
     },
 ]
 
 let index = 0
 
-function displayQuestions(){
+function displayQuestions() {
     document.getElementById("displayQuestions").innerHTML = allQuestion[index].question
 
     let btn = ""
     allQuestion[index].option.forEach((opt) => {
-      btn += `  <button class="btn btn-outline-warning text-dark" > ${opt.answer} </button>`
+        btn += `  <button class="btn btn-outline-warning text-dark" > ${opt.answer} </button>`
     })
     document.getElementById("btn").innerHTML = btn
-   
+
 }
 
-function next(){
-   index++
-   document.getElementById("displayquestions").innerHTML = allQuestion[index].question
-   displayQuestions()
+ 
+ 
+ 
+ function next() {
+    index += 1
+    document.getElementById("displayQuestions").innerHTML = allQuestion[index].question
+    displayQuestions()
 }
 
-function back(){
-   index--
-   document.getElementById("displayquestions").innerHTML = allQuestion[index].question
-   displayQuestions()
+function back() {
+    index -= 1
+    document.getElementById("displayQuestions").innerHTML = allQuestion[index].question
+    displayQuestions()
 }
 
 displayQuestions()
-
